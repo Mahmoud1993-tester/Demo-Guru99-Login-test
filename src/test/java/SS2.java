0d044e5a39d6f8523b15243d3b41fe72e1aa2227
Mahmoud1993-tester <msaber93.egy@gmail.com>
2024-12-08 14:53:43 +0200

    Add invalid Tests

---
+++ src/test/java/SS2.java
@@ -0,0 +1,52 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class SS2 {
+    WebDriver driver;
+    String baseUrl = "https://www.demo.guru99.com/V4/";
+
+    @BeforeTest
+    public void prepare() {
+        System.setProperty("web driver.gecko.driver", "D:\\geckodriver-v0.35.0-win32\\geckodriver.exe");
+        driver = new FirefoxDriver();
+    }
+
+    @AfterTest
+    public void closeBrowser() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+
+    @Test (priority = 0)
+    public void launchBrowser() {
+        driver.get(baseUrl);
+    }
+
+
+    @Test (priority = 1)
+    public void EnterUsername() {
+        driver.findElement(By.name("uid")).sendKeys("mahmoud");
+    }
+
+    @Test (priority = 2)
+    public void enterPassword() {
+        driver.findElement(By.name("password")).sendKeys("AgUtAzY");
+    }
+    @Test (priority = 3)
+    public void clickLogin() {
+        driver.findElement(By.name("btnLogin")).click();
+    }
+
+}
+
+
+
---
+++ src/test/java/SS4.java
@@ -0,0 +1,51 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class SS4 {
+    WebDriver driver;
+    String baseUrl = "https://www.demo.guru99.com/V4/";
+
+    @BeforeTest
+    public void prepare() {
+        System.setProperty("web driver.gecko.driver", "D:\\geckodriver-v0.35.0-win32\\geckodriver.exe");
+        driver = new FirefoxDriver();
+    }
+
+    @AfterTest
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+    @Test (priority = 0)
+    public void launchBrowser() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.get(baseUrl);
+    }
+
+
+    @Test (priority = 1)
+    public void EnterUsername() {
+        driver.findElement(By.name("uid")).sendKeys("mahmoud");
+    }
+
+    @Test (priority = 2)
+    public void enterPassword() {
+        driver.findElement(By.name("password")).sendKeys("A445500");
+    }
+    @Test (priority = 3)
+    public void clickLogin() {
+        driver.findElement(By.name("btnLogin")).click();
+    }
+
+}
+
+
